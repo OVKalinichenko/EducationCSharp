b43bd136611d069d738fbf67b59e1a8b50619c71
Olga_Kalinichenko <okalinichenko@icloud.com>
2023-09-21 16:33:40 +0200

    Addea folder Homewok6 (Task2_21.09.23)

---
+++ Homeworks/Homework6/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+void ThirdDigit(int num)
+{
+    int comp = 1000;
+    int count = 1;
+    if(num < 100)
+    {
+        System.Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        while(comp < 1000000000)
+        {
+            if(num < comp)
+            {
+                System.Console.WriteLine((num / count) % 10);
+                break;
+            }
+            else{
+                comp = comp * 10;
+                count = count * 10;
+            }
+        }
+    }
+}
+
+Console.Write("Enter a digit: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ThirdDigit(number);
