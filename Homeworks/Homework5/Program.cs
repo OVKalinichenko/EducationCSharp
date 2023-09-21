cdbb1b08d2ac2e624c380918662b6171d4e2e261
Olga_Kalinichenko <okalinichenko@icloud.com>
2023-09-21 16:01:54 +0200

    Addea Homewok5 (Task1_21.09.23)

---
+++ Homeworks/Homework5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+
+void TensDigit(int num)
+{
+  int un = num % 10;
+  int ten = (num - un) % 100;
+  Console.WriteLine(ten / 10);
+}
+
+Console.Write("Enter tree-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+TensDigit(number);
