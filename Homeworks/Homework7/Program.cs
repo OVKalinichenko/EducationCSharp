65a5880846ffd4f34f41c067ab7a25fb60edb3f8
Olga_Kalinichenko <okalinichenko@icloud.com>
2023-09-21 16:53:54 +0200

    Added folder Homewok7 (Task3_21.09.23)

---
+++ Homeworks/Homework7/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 1 -> нет
+
+bool DayOfTheWeeK(int num)
+{
+    if(num < 6)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+Console.Write("Enter a week-day number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 7)
+{
+    System.Console.WriteLine("Wrong number. True again.");
+}
+else
+{
+    System.Console.WriteLine($"Is it a weekend -> {DayOfTheWeeK(number)}");
+}
